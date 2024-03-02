6366cee45e89cd22bbcc5964ba42b147619a0c0b
chloejuanedalaborie <chloe2007.jl@gmail.com>
2024-03-02 11:05:07 +0100

    ajout des fichier + affichage du plan avec les check non cliquable

--- test.py
+++ test.py
@@ -1,27 +1,53 @@
-# Example file showing a basic pygame "game loop"
 import pygame
 
-# pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+fenetre = pygame.display.set_mode((640, 480))
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+fond = pygame.image.load("plan_lycee.jpg").convert()
+fenetre.blit(fond,(0,0))
+
+check = pygame.image.load("check.png").convert_alpha()
+mask_check = pygame.mask.from_surface(check)
+rect_check = check.get_rect()
+rect_check.move_ip(400,300)
+fenetre.blit(check, rect_check)
+
+bicheck = pygame.image.load("check.png").convert_alpha()
+mask_bicheck = pygame.mask.from_surface(bicheck)
+rect_bicheck = bicheck.get_rect()
+rect_bicheck.move_ip(400,140)
+fenetre.blit(bicheck, rect_bicheck)
+
+main = pygame.image.load("hand.png").convert_alpha()
+mask_main = pygame.mask.from_surface(main)
+rect_check.move_ip(10,10)
+rect_bicheck.move_ip(10,10)
+rect_main = main.get_rect()
+
+clock = pygame.time.Clock()
+continuer = True
+
+
+while continuer :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
+            continuer = False
+        elif event.type == pygame.MOUSEMOTION:
+            fenetre.blit(fond, rect_main, rect_main)
+            fenetre.blit(check, (400,300))
+            fenetre.blit(bicheck, (400, 140))
+            rect_main.center = event.pos
+            fenetre.blit(main, rect_main)
+    
+    offset_x = rect_check.x - rect_main.x
+    offset_y = rect_check.y - rect_main.y
+    if mask_main.overlap(mask_check, (offset_x, offset_y)):
+        print("Touché")
+    
+    else:
+        print("")
+    
+    pygame.display.update()
+    clock.tick(40)
 
 pygame.quit()
