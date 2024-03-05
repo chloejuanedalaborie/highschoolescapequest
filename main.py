12b922a74a5a8182b56a4c597859a94dea5d9150
chloejuanedalaborie <chloe2007.jl@gmail.com>
2024-03-05 21:56:05 +0100

    ancienne version restaurée

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-
 pygame.init()
+
 pygame.display.set_caption("High School Escape Quest")
 fenetre = pygame.display.set_mode((600, 600))
 
@@ -21,23 +21,24 @@ mask_main = pygame.mask.from_surface(main)
 rect_play.move_ip(10,10)
 rect_main = main.get_rect()
 
-rect_surf = pygame.Surface(rect_play.size).set_alpha(0) #zone cliquable
-
-
+rect_surf = pygame.Surface(rect_play.size).set_alpha(0) #zone cliquable du play acceuil
 
 clock = pygame.time.Clock()
 continuer = True
 
 
 while continuer :
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             continuer = False
+            
         elif event.type == pygame.MOUSEMOTION:
             fenetre.blit(fond, rect_main, rect_main)
             fenetre.blit(play, (250,450))
             rect_main.center = event.pos
             fenetre.blit(main, rect_main)
+            
         elif event.type == pygame.MOUSEBUTTONDOWN:
             #lorsque le clique de la souris est appuyé sur la surface rectangulaire du play, alors le fond change et le play disparait
             if event.button == 1: # 1= clique gauche
@@ -46,6 +47,9 @@ while continuer :
                     fenetre.blit(fond,(0,0))                    
                     play = pygame.Surface(rect_play.size) #rend le play transparent
                     play.set_alpha(0) #rend le carré noir du rect_play transparent
+                    
+                                
+                        
     
     pygame.display.flip()
     
